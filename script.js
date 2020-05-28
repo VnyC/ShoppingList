625120b616a7502252761c59223a4fbdc0a7793c
Vinay C <65414132+VnyC@users.noreply.github.com>
2020-05-29 00:18:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,85 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var b = document.getElementById("addClick");
+var n= document.getElementById("newItem");
+var ul= document.getElementById("ol");
+var list= document.getElementsByTagName("li");
+var newL = document.getElementById("newL");
 
-function inputLength() {
-	return input.value.length;
-}
+function Ipl (){
+	return n.value.length;
+};
 
-function createListElement() {
+
+
+
+function adder (){
 	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-}
+	var newButton = document.createElement("BUTTON")
+		li.appendChild(document.createTextNode(n.value));
+		li.appendChild(newButton);
+		newButton.appendChild(document.createTextNode("Check"));
+		ul.appendChild(li);
+		li.setAttribute("class","dash")
+		newButton.setAttribute("id","newButton")
+		n.value="";
+		newButton.addEventListener("click", function (){
+		li.classList.toggle("done"); 	});
+};
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
+
+
+
+function placeholder() {
+	n.placeholder="Enter Item";
+};
+
+function ent () {
+	if (event.keyCode === 13){
+		return true;
 	}
 }
 
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
+function refbut(){
+	if(list.length > 0){	
+	var newL = document.getElementById("newL");
+	newL.innerHTML="New List" ;
+	newL.addEventListener("click", function removelist(){
+		const parent = document.getElementById("ol");
+		while (parent.firstChild) {
+		parent.firstChild.remove();
+		}
+} ); }
+};
+
+function removebut(){
+	var rembutex = document.getElementsByTagName("li");
+	if(rembutex === true){
+		console.log("yes");
 	}
-}
+	};
 
-button.addEventListener("click", addListAfterClick);
 
-input.addEventListener("keypress", addListAfterKeypress);
+
+function cl () {
+	if (Ipl() > 0) {
+		adder();
+		refbut();
+		removebut();
+	} else{
+		placeholder();
+	}
+};
+
+function ce () {
+	if (Ipl() > 0 && ent()) {
+		adder();
+		refbut();
+		removebut();
+	} else{
+		placeholder();
+	}
+};
+
+b.addEventListener("click", cl);
+
+n.addEventListener("keypress", ce);
+
